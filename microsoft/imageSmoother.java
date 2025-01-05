1cfb1c8fcf13fde55a1e7344d74965a7b458b9c0
Deepak Singh <71811739+deepaksingh128@users.noreply.github.com>
2025-01-05 10:50:19 +0530

    Create imageSmoother.java

---
+++ microsoft/imageSmoother.java
@@ -0,0 +1,83 @@
+//Approach-2
+//T.C : O(m*n)
+//S.C : O(n)
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+    
+        int m = img.length;
+        int n = img[0].length;
+
+        int[] temp = new int[n];
+        int prevCorner = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                int sum = 0;
+                int count = 0;
+
+                // Bottom neighbors
+                if (i + 1 < m) {
+                    if (j - 1 >= 0) {
+                        sum += img[i + 1][j - 1];
+                        count += 1;
+                    }
+                    sum += img[i + 1][j];
+                    count += 1;
+                    if (j + 1 < n) {
+                        sum += img[i + 1][j + 1];
+                        count += 1;
+                    }
+                }
+
+                // Next neighbor
+                if (j + 1 < n) {
+                    sum += img[i][j + 1];
+                    count += 1;
+                }
+
+                // This cell
+                sum += img[i][j];
+                count += 1;
+
+                // Previous neighbor
+                if (j - 1 >= 0) {
+                    sum += temp[j - 1];
+                    count += 1;
+                }
+
+                // Top neighbors
+                if (i - 1 >= 0) {
+                    // Left-top corner-sharing neighbor.
+                    if (j - 1 >= 0) {
+                        sum += prevCorner;
+                        count += 1;
+                    }
+
+                    // Top edge-sharing neighbor.
+                    sum += temp[j];
+                    count += 1;
+
+                    // Right-top corner-sharing neighbor.
+                    if (j + 1 < n) {
+                        sum += temp[j + 1];
+                        count += 1;
+                    }
+                }
+
+                // store corner value
+                if (i - 1 >= 0) {
+                    prevCorner = temp[j];
+                }
+
+                // Store current value
+                temp[j] = img[i][j];
+
+                // Overwrite with smoothed value.
+                img[i][j] = sum / count;
+            }
+        }
+        
+        return img;
+    }
+}

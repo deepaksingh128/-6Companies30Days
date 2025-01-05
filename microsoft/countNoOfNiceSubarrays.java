48c613e59e35b6b4f8bbc12d552997603fd30172
Deepak Singh <71811739+deepaksingh128@users.noreply.github.com>
2025-01-05 12:29:00 +0530

    Update countNoOfNiceSubarrays.java

--- microsoft/countNoOfNiceSubarrays.java
+++ microsoft/countNoOfNiceSubarrays.java
@@ -23,3 +23,41 @@ class Solution {
         return result;
     }
 }
+
+
+
+// Method 2 :- Using sliding window
+
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+
+        int prevCount = 0;
+        int oddCount = 0;
+        int result = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while (j < n) {
+            if (nums[j] % 2 != 0) {
+                oddCount ++;
+                prevCount = 0;
+            }
+
+            while (oddCount == k) {
+                prevCount ++;
+
+                if (nums[i] % 2 == 1) {
+                    oddCount --;
+                }
+                i ++;
+            }
+
+            result += prevCount;
+            j ++;
+        }
+
+        return result;
+    }
+}
